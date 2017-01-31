e1bd306f36ba371e06ea03c3fe235628f7007707
Cuihtlauac ALVARADO <cuihtlauac.alvarado@orange.com>
2017-01-31 09:36:25 +0100

    Remove menu

--- app/src/main/java/com/orang/nstdemo/Activity.java
+++ app/src/main/java/com/orang/nstdemo/Activity.java
@@ -1,19 +1,12 @@
 package com.orang.nstdemo;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class Activity extends android.app.Activity {
-
-    private static final int PERMISSION_ID = 1;
 
     Helper helper;
     ListView listView ;
@@ -22,7 +15,7 @@ public class Activity extends android.app.Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity);
 
         helper = new Helper(this);
 
@@ -37,48 +30,17 @@ public class Activity extends android.app.Activity {
     @Override
     protected void onResume() {
         super.onResume();
-    if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_DENIED) {
+        if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_DENIED) {
             requestPermissions(new String[] { Manifest.permission.READ_PHONE_STATE }, 0);
         }
         if (checkSelfPermission(Manifest.permission.ACCESS_NETWORK_STATE) == PackageManager.PERMISSION_DENIED) {
             requestPermissions(new String[] { Manifest.permission.ACCESS_NETWORK_STATE }, 0);
         }
         adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, helper.getListOfRecords());
+                                           android.R.layout.simple_list_item_1, android.R.id.text1, helper.getListOfRecords());
 
         listView.setAdapter(adapter);
 
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (requestCode == PERMISSION_ID && grantResults[0] ==
-                    PackageManager.PERMISSION_GRANTED) {
-                helper.addNewAlarm();
-                Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
-                this.startActivity(intent);
-            }
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu, menu);//Menu Resource, Menu
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.add:
-                for(int i=0; i < 10 ; i++)
-                    helper.addNewQuery();
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
 }
