13abf03290cfa661ff286b785fa81a7d0479c7ef
Cuihtlauac ALVARADO <cuihtlauac.alvarado@orange.com>
2017-02-01 11:41:50 +0100

    Remove alarm toast

--- app/src/main/java/com/orang/nstdemo/AlarmReceiver.java
+++ app/src/main/java/com/orang/nstdemo/AlarmReceiver.java
@@ -3,17 +3,14 @@ package com.orang.nstdemo;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
 
 public class AlarmReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
         Helper helper = Helper.getSharedInstance(context);
         helper.addNewAlarm();
         helper.addNewQuery();
-
-        Toast.makeText(context, "I'm running", Toast.LENGTH_SHORT).show();
     }
+
 }
